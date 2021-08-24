9eebbb7cd31913a2099ac515019671feda53db58
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-24 17:08:03 -0500

    Change the install root

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ prefix=${PREFIX:-/usr/local}
 
 cd "$prefix"
 
-install -Dt "$prefix/bin" "$src_dir/bin/$name"
-sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/bin/$name"
+install -Dt "$prefix/usr/bin" "$src_dir/bin/$name"
+sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/usr/bin/$name"
 
-install -Dt "$prefix/lib/$name" "$src_dir/lib/$name"/*
+install -Dt "$prefix/usr/lib/$name" "$src_dir/lib/$name"/*
 
-install -Dt "$prefix/share/$name" "$src_dir/share/$name"/*
+install -Dt "$prefix/usr/share/$name" "$src_dir/share/$name"/*
 
-install -Dt "$prefix/share/man/man5" "$src_dir/share/man/man5"/*
-install -Dt "$prefix/share/man/man8" "$src_dir/share/man/man8"/*
+install -Dt "$prefix/usr/share/man/man5" "$src_dir/share/man/man5"/*
+install -Dt "$prefix/usr/share/man/man8" "$src_dir/share/man/man8"/*
 
-install -Dt "$prefix/lib/systemd/system" "$src_dir/lib/systemd/system"/*
-sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/lib/systemd/system/$name"-*.service
+install -Dt "$prefix/usr/lib/systemd/system" "$src_dir/lib/systemd/system"/*
+sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/usr/lib/systemd/system/$name"-*.service
 
-install -Dt "$prefix/share" "$src_dir"/{README,LICENSE}.md
+install -Dt "$prefix/usr/share" "$src_dir"/{README,LICENSE}.md
