85bab1a57b1a078dc5f82afe4d4a5da80e44a7a1
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-24 11:50:31 -0500

    Reduce verbosity

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ prefix=${PREFIX:-/usr/local}
 
 cd "$prefix"
 
-install -v -Dt "$prefix/bin" "$src_dir/bin/$name"
+install -Dt "$prefix/bin" "$src_dir/bin/$name"
 sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/bin/$name"
 
-install -v -Dt "$prefix/lib/$name" "$src_dir/lib/$name"/*
+install -Dt "$prefix/lib/$name" "$src_dir/lib/$name"/*
 
-install -v -Dt "$prefix/share/$name" "$src_dir/share/$name"/*
+install -Dt "$prefix/share/$name" "$src_dir/share/$name"/*
 
-install -v -Dt "$prefix/share/man/man5" "$src_dir/share/man/man5"/*
-install -v -Dt "$prefix/share/man/man8" "$src_dir/share/man/man8"/*
+install -Dt "$prefix/share/man/man5" "$src_dir/share/man/man5"/*
+install -Dt "$prefix/share/man/man8" "$src_dir/share/man/man8"/*
 
-install -v -Dt "$prefix/lib/systemd/system" "$src_dir/lib/systemd/system"/*
+install -Dt "$prefix/lib/systemd/system" "$src_dir/lib/systemd/system"/*
 sed -i ":/usr/local:s:/usr/local:$prefix:" "$prefix/lib/systemd/system/$name"-*.service
