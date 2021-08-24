7ae1541746f351a71a978fa1abb4d3d4fc0465a4
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-24 11:52:53 -0500

    Reduce verbosity

--- test.sh
+++ test.sh
@@ -13,5 +13,4 @@ export BTRFS_SNAPSHOTS_CONFIG_FILE=$PWD/etc/main.conf
 export BTRFS_SNAPSHOTS_PROFILE_DIR=$PWD/etc/profiles.d
 export BTRFS_SNAPSHOTS_BYPASS_IS_MOUNTED=1
 
-find ./tests/{unit,integration} -type f -executable "$@" |
-    parallel -j0 'echo "running test: {}"; "{}" || echo "test failed: {}"'
+find ./tests/{unit,integration} -type f -executable "$@" | parallel -j0
