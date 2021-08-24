fc4676cc1023133f4e07ef54b2356cf4fe4bc280
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-24 17:54:13 -0500

    Change the install root

--- install.sh
+++ install.sh
@@ -10,14 +10,9 @@ mkdir -p "$dst_dir"
 cd "$dst_dir"
 
 install -Dt ./bin "$src_dir/bin/$pkg_name"
-
 install -Dt ./lib/"$pkg_name" "$src_dir/lib/$pkg_name"/*
-
 install -Dt ./share/"$pkg_name" "$src_dir/share/$pkg_name"/*
-
 install -Dt ./share/man/man5 "$src_dir/share/man/man5"/*
 install -Dt ./share/man/man8 "$src_dir/share/man/man8"/*
-
 install -Dt ./lib/systemd/system "$src_dir/lib/systemd/system"/*
-
 install -Dt ./share "$src_dir"/{README,LICENSE}.md
