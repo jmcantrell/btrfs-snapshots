621701ff96305e2a42b7aa5f7c0da9520ed6f782
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-28 11:26:37 -0500

    Rename some library files

--- lib/btrfs-snapshots/message.sh
+++ lib/btrfs-snapshots/message.sh
@@ -1,40 +1,36 @@
-format() {
-    local message=$1
-    shift
+export TEXT_USAGE="Manage timestamped collections of btrfs snapshots.
 
-    local arg
-    for arg in "$@"; do
-        if [[ $arg == *=* ]]; then
-            local key=${arg%%=*}
-            local value=${arg#*=}
-            message=${message//%$key%/$value}
-        else
-            declare -n var=$arg
-            message=${message//%$arg%/${var[*]}}
-        fi
-    done
+Usage: btrfs-snapshots [OPTIONS] ACTION
 
-    printf "%s" "$message"
-}
+Options:
+    -h         show this help message
+    -n         only report what would happen (no changes are made)
+    -p NAME    only perform ACTION on the NAME profile (default: all)
 
-info() {
-    local message
-    message=$(format "$@")
-    printf "%s\n" "$message" >&2
-}
+Arguments:
+    ACTION     perform ACTION on the selected profiles (default: list)
 
-error() {
-    local message=$1
-    shift
-    info "ERROR: $message" "$@"
-}
+Actions:
+    list       print the full path of every recognized snapshot
+    create     create a new timestamped snapshot in the configured location
+    prune      delete any snapshots that fall outside of the defined limits
+"
 
-die() {
-    (($# > 0)) && error "$@"
-    exit 1
-}
+export TEXT_INVALID="Invalid %NAME%: %VALUE%"
 
-finish() {
-    (($# > 0)) && info "$@"
-    exit 0
-}
+export TEXT_PROFILE_NOT_EXIST="Profile does not exist: %PROFILE_NAME%"
+export TEXT_PROFILE_VARIABLE_NOT_SET="Invalid profile '%PROFILE_NAME%': %VARIABLE% is not set"
+export TEXT_PROFILE_ABS_PATH="Invalid profile '%PROFILE_NAME%': %VARIABLE% must be an absolute path"
+export TEXT_PROFILE_SNAPSHOTS_UNIQUE="Invalid profile '%PROFILE_NAME%': SNAPSHOTS value is already defined in another profile"
+export TEXT_PROFILE_FAILED="Unable to load profile '%PROFILE_FILE%'"
+
+export TEXT_DEFAULTS_SUBVOLUME="Invalid defaults: SUBVOLUME must be set in the profile configuration"
+export TEXT_DEFAULTS_SNAPSHOTS_MISSING_NAME="Invalid defaults: SNAPSHOTS is missing %NAME% placeholder"
+export TEXT_DEFAULTS_FAILED="Unable to load defaults: %DEFAULTS_FILE%"
+
+export TEXT_ACTION="Performing action '%ACTION%' for profile '%PROFILE_NAME%'"
+export TEXT_ACTION_FAILED="Unable to perform action '%ACTION%'"
+
+export TEXT_CREATE_SUBVOLUME_MISSING="Skipping profile '%PROFILE_NAME%': subvolume is missing"
+
+export TEXT_BTRFS_FAILED="The btrfs command errored with status code '%STATUS%'"
--- tests/lib/init.sh
+++ tests/lib/init.sh
@@ -1,4 +1,4 @@
+. "$TESTS_LIB_DIR"/io.sh
 . "$TESTS_LIB_DIR"/temp.sh
 . "$TESTS_LIB_DIR"/assert.sh
 . "$TESTS_LIB_DIR"/timestamp.sh
-. "$TESTS_LIB_DIR"/message.sh
