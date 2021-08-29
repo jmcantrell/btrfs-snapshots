f70bdeca92dfea27101df03823ee3167f45e7ffa
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-29 18:23:41 -0500

    Change example config file location

--- install.sh
+++ install.sh
@@ -10,26 +10,28 @@ mkdir -p "$dst_dir"
 cd "$dst_dir"
 
 install -m755 -Dt ./bin \
-    "$src_dir/bin/$pkg_name"
+    "$src_dir"/bin/"$pkg_name"
 
 install -m644 -Dt ./lib/"$pkg_name" \
-    "$src_dir/lib/$pkg_name"/*
+    "$src_dir"/lib/"$pkg_name"/*
 
 install -m644 -Dt ./share/man/man5 \
-    "$src_dir/share/man/man5"/*
+    "$src_dir"/share/man/man5/*
 
 install -m644 -Dt ./share/man/man8 \
-    "$src_dir/share/man/man8"/*
+    "$src_dir"/share/man/man8/*
 
 install -m644 -Dt ./lib/systemd/system \
-    "$src_dir/lib/systemd/system"/*
+    "$src_dir"/lib/systemd/system/*
+
+install -m644 -Dt ./share/doc/"$pkg_name"/config \
+    "$src_dir"/share/doc/"$pkg_name"/config/* \
 
 install -m644 -Dt ./share/doc/"$pkg_name" \
-    "$src_dir/share/doc/$pkg_name"/* \
     "$src_dir"/{README,LICENSE}.md
 
-install -m644 -Dt ./share/bash-completion/completions \
-    "$src_dir/share/bash-completion/completions"/*
+install -m644 -Dt ./share/zsh/site-functions \
+    "$src_dir"/share/zsh/site-functions/*
 
-install -m644 -Dt ./share/zsh/functions/Completion/Linux \
-    "$src_dir/share/zsh/functions/Completion/Linux"/*
+install -m644 -Dt ./share/bash-completion/completions \
+    "$src_dir"/share/bash-completion/completions/*
