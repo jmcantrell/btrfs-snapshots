2580650ff85e2cdfd79137ef0167bdb95e7faa2d
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-31 01:05:35 -0500

    Slight changes to message punctuation

--- lib/btrfs-snapshots/message.sh
+++ lib/btrfs-snapshots/message.sh
@@ -20,21 +20,21 @@ Actions:
 If no profiles are specified with -p, then every profile is selected.
 "
 
-export TEXT_INVALID="Invalid %NAME%: %VALUE%"
+export TEXT_INVALID="Invalid %NAME% '%VALUE%'"
 
-export TEXT_PROFILE_NOT_EXIST="Profile does not exist: %PROFILE_NAME%"
-export TEXT_PROFILE_VARIABLE_NOT_SET="Invalid profile '%PROFILE_NAME%': %VARIABLE% is not set"
-export TEXT_PROFILE_ABS_PATH="Invalid profile '%PROFILE_NAME%': %VARIABLE% must be an absolute path"
-export TEXT_PROFILE_SNAPSHOTS_UNIQUE="Invalid profile '%PROFILE_NAME%': SNAPSHOTS value is already defined in another profile"
+export TEXT_PROFILE_NOT_EXIST="Profile '%PROFILE_NAME%' does not exist"
+export TEXT_PROFILE_VARIABLE_NOT_SET="Invalid profile '%PROFILE_NAME%'; %VARIABLE% is not set"
+export TEXT_PROFILE_ABS_PATH="Invalid profile '%PROFILE_NAME%'; %VARIABLE% must be an absolute path"
+export TEXT_PROFILE_SNAPSHOTS_UNIQUE="Invalid profile '%PROFILE_NAME%'; SNAPSHOTS value is already defined in another profile"
 export TEXT_PROFILE_FAILED="Unable to load profile '%PROFILE_FILE%'"
 
-export TEXT_DEFAULTS_SUBVOLUME="Invalid defaults: SUBVOLUME must be set in the profile configuration"
-export TEXT_DEFAULTS_SNAPSHOTS_MISSING_NAME="Invalid defaults: SNAPSHOTS is missing %NAME% placeholder"
-export TEXT_DEFAULTS_FAILED="Unable to load defaults: %DEFAULTS_FILE%"
+export TEXT_DEFAULTS_SUBVOLUME="Invalid defaults; SUBVOLUME must be set in the profile configuration"
+export TEXT_DEFAULTS_SNAPSHOTS_MISSING_NAME="Invalid defaults; SNAPSHOTS is missing %NAME% placeholder"
+export TEXT_DEFAULTS_FAILED="Unable to load defaults '%DEFAULTS_FILE%'"
 
 export TEXT_ACTION="Performing action '%ACTION%' for profile '%PROFILE_NAME%'"
 export TEXT_ACTION_FAILED="Unable to perform action '%ACTION%'"
 
-export TEXT_CREATE_SUBVOLUME_MISSING="Skipping profile '%PROFILE_NAME%': subvolume is missing"
+export TEXT_CREATE_SUBVOLUME_MISSING="Skipping profile '%PROFILE_NAME%'; subvolume is missing"
 
 export TEXT_BTRFS_FAILED="The btrfs command errored with status code '%STATUS%'"
