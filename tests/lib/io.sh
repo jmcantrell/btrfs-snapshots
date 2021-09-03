667a8df98ad721bb908487dad071d51ed8843a84
Jeremy Cantrell <jmcantrell@gmail.com>
2021-09-03 08:08:06 -0500

    Remove unused function

--- tests/lib/io.sh
+++ tests/lib/io.sh
@@ -1,15 +1,12 @@
-debug() {
-    for var in "$@"; do
-        printf "%s=%s\n" "$var" "${!var}"
-    done
-}
-
-die() {
+trace() {
     local i size=${#FUNCNAME[@]}
     for ((i = size - 1; i >= 0; i--)); do
         printf "%s\n" "${BASH_SOURCE[i]}: line ${BASH_LINENO[i - 1]}: ${FUNCNAME[i]}" >&2
     done
+}
 
-    error "$@"
+die() {
+    trace
+    (($# > 0)) && error "$@"
     exit 1
 }
