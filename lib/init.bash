3ac76356d40997f4caa85954f1a6aa2a680aa412
Jeremy Cantrell <jmcantrell@gmail.com>
2022-09-02 18:49:59 -0500

    Rename profile lib to config

--- lib/init.bash
+++ lib/init.bash
@@ -3,8 +3,8 @@ export TIMESTAMP_FORMAT="%Y-%m-%dT%H:%M:%SZ"
 export TIMESTAMP_PATTERN="^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z"
 
 . "$LIB_DIR"/action.bash
+. "$LIB_DIR"/config.bash
 . "$LIB_DIR"/limit.bash
 . "$LIB_DIR"/locale.bash
-. "$LIB_DIR"/profile.bash
 . "$LIB_DIR"/snapshot.bash
 . "$LIB_DIR"/timestamp.bash
