b6bc55738ca61d76e50aafa22edb5d544a70fb2e
Jeremy Cantrell <jmcantrell@gmail.com>
2022-11-06 02:31:46 -0600

    Update completion files

--- completion/btrfs-snapshots.zsh
+++ completion/btrfs-snapshots.zsh
@@ -1,24 +1,27 @@
 #compdef btrfs-snapshots
 
-local state state_descr context line opt_args
+local context state state_descr line opt_args
 
 _arguments -s -S -A "-*" : \
     "(- : *)-h[show the help text]" \
-    "(-h)-p[select a profile]:profile:->profile" \
-    "(-h)-C[set the configuration directory]:configuration directory:_files -/" \
-    '(-h)::action:((
+    "-p[select a profile]:profile:->profile" \
+    "-C[set the configuration directory]:configuration directory:_files -/" \
+    '1::action:((
         "create:create a new snapshot"
         "prune:delete old snapshots"
     ))' \
     && return
 
-if [[ $state == profile ]]; then
+case $state in
+profile)
     local config_dir
     if (( $+opt_args[-C] )); then
         config_dir=$opt_args[-C]
     else
         config_dir=${BTRFS_SNAPSHOTS_CONFIG_DIR:-/usr/local/etc/btrfs-snapshots}
     fi
+
     local -a profile_names=($config_dir/profile.d/*.conf(N:r:t))
     (($#profile_names)) && _values 'profiles' $profile_names
-fi
+    ;;
+esac
