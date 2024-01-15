30250a78a602979dbb183e0e61f048f4d2535dfb
Jeremy Cantrell <jmcantrell@gmail.com>
2024-01-14 22:41:49 -0600

    Update completion to match recent simplification

--- completion/btrfs-snapshots.bash
+++ completion/btrfs-snapshots.bash
@@ -1,84 +1,29 @@
-__find_array_value() {
-    local needle=$1
-    shift
-
-    local i=0
-
-    while (($# > 0)); do
-        local straw=$1
-        shift
-
-        if [[ $straw == "$needle" ]]; then
-            printf "%s\n" "$i"
-            return 0
-        fi
-
-        i=$((i + 1))
-    done
-
-    return 1
-}
-
-__get_opt_arg() {
-    local opt=$1
-    shift 1
-
-    local -a words=("$@")
-
-    local i
-    if i=$(__find_array_value "$opt" "${words[@]}"); then
-        printf "%s\n" "${words[i + 1]}"
-        return 0
-    fi
-
-    return 1
-}
-
-__shallow_basenames() {
-    local dir=$1
-    local ext=${2:-}
-
-    [[ -d $dir ]] || return 0
-
-    for file in "$dir"/*"${ext:+$ext}"; do
-        if [[ -f $file ]]; then
-            name=${file##*/}
-            name=${name%"$ext"}
-            printf "%s\n" "$name"
-        fi
-    done
-}
-
 _btrfs_snapshots() {
     local cur prev words
     _init_completion || return
 
-    local config_dir
-    if ! config_dir=$(__get_opt_arg -C "${words[@]}"); then
-        config_dir=${BTRFS_SNAPSHOTS_CONFIG_DIR:-/usr/local/etc/btrfs-snapshots}
-    fi
+    local profiles_dir=${BTRFS_SNAPSHOTS_CONFIG_DIR:-/usr/local/etc/btrfs-snapshots}/profile.d
 
-    local profiles_dir=$config_dir/profile.d
-
-    case $prev in
-    -C)
-        _filedir -d
-        return
-        ;;
-    -p)
-        local -a profile_names
-        readarray -t profile_names < <(__shallow_basenames "$profiles_dir" ".conf")
-        readarray -t COMPREPLY < <(compgen -W "${profile_names[*]}" -- "$cur")
-        return
-        ;;
-    esac
+    local options=(-h)
+    local actions=(create prune)
 
     case $cur in
     -*)
-        readarray -t COMPREPLY < <(compgen -W '-h -p -C' -- "$cur")
+        readarray -t COMPREPLY < <(compgen -W "${options[*]}" -- "$cur")
         ;;
     *)
-        readarray -t COMPREPLY < <(compgen -W "create prune" -- "$cur")
+        if ((${#words[@]} >= 3)); then
+            local profile_name profile_names=()
+            for profile_file in "$profiles_dir"/*.conf; do
+                profile_name=${profile_file##*/}
+                profile_name=${profile_name%.conf}
+                profile_names+=("$profile_name")
+            done
+
+            readarray -t COMPREPLY < <(compgen -W "${profile_names[*]}" -- "$cur")
+        else
+            readarray -t COMPREPLY < <(compgen -W "${actions[*]}" -- "$cur")
+        fi
         ;;
     esac
 }
--- completion/btrfs-snapshots.zsh
+++ completion/btrfs-snapshots.zsh
@@ -1,32 +1,24 @@
 #compdef btrfs-snapshots
 
-local context state state_descr line opt_args
+local curcontext=$curcontext context state state_descr line opt_args
 
-_arguments -s -S -A "-*" : \
+_arguments -C -s -S -A "-*" : \
     "(- : *)-h[show the help text]" \
-    "-p[select a profile]:profile:->profile" \
-    "-C[set the configuration directory]:configuration directory:_files -/" \
-    '1::action:((
+    ':action:((
         "create\:create a new snapshot"
         "prune\:delete old snapshots"
     ))' \
+    "*: :->profile" \
     && return
+
+service=$line[1]
+curcontext=${curcontext%:*}-$service
 
 case $state in
 profile)
-    local config_dir
-    if (( $+opt_args[-C] )); then
-        config_dir=$opt_args[-C]
-    else
-        config_dir=${BTRFS_SNAPSHOTS_CONFIG_DIR:-/usr/local/etc/btrfs-snapshots}
-    fi
-
-    local -a profile_names=($config_dir/profile.d/*.conf(N:r:t))
-    if (($#profile_names)); then
-        _values 'profiles' $profile_names && return
-    else
-        _message 'no profiles'
-    fi
+    local config_dir=${BTRFS_SNAPSHOTS_CONFIG_DIR:-/usr/local/etc/btrfs-snapshots}
+    local expl names=($config_dir/profile.d/*.conf(N:r:t))
+    _wanted values expl "profile" compadd -a names && return
     ;;
 esac
 
