811744fa6934dfe958b4f84fa768d1dcbf781083
Jeremy Cantrell <jmcantrell@gmail.com>
2026-09-07 23:25:23 -0500

    build(pkg): overhaul path rewriting
    Before this change, source files that reference other source files did
    so using exported environment variables that defaulted to being in
    `/usr/local` and could be overridden for development.
    
    These changes simplify the situation a bit, making it a bit easier to
    reason about. Now, these references default to a relative path for
    development with no overrides (unless it's part of the functionality,
    like overriding the configuration directory).
    
    During packaging, the paths are rewritten to system paths like before,
    but the rewriting is no longer optional. Before, the assumption was that
    the prefix would be `/usr/local` unless rewritten. Now, packaging is not
    viable unless path rewriting is done explicitly.

--- tests/lib/init.bash
+++ tests/lib/init.bash
@@ -1,12 +1,11 @@
-export CONFIG_DIR=$TEMP_DIR/config
+export CONFIG_DIR=$TEMP_DIR/etc
 export PROFILES_DIR=$CONFIG_DIR/profile.d
 export DEFAULTS_FILE=$CONFIG_DIR/defaults.conf
 
-export BTRFS_SNAPSHOTS_LIB_DIR=$LIB_DIR
 export BTRFS_SNAPSHOTS_CONFIG_DIR=$CONFIG_DIR
 
 export SELF_NAME=test
 export TIMESTAMP=2001-01-01T00:00:00Z
 
-source "$TESTS_LIB_DIR"/assert.bash
-source "$TESTS_LIB_DIR"/timestamp.bash
+source ./tests/lib/assert.bash
+source ./tests/lib/timestamp.bash
