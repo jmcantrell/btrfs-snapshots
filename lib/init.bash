2fd038565df1d15a77f88dff11ae1a6b1a07f758
Jeremy Cantrell <jmcantrell@gmail.com>
2026-09-08 02:21:25 -0500

    build(pkg): account for testing after path rewriting
    I failed to see what would happen if tests were run on file who have had
    their paths rewritten. They referred to files already installed to the
    system. Luckily this failure was visible.
    
    I've restored some of the original environment variable overrides so
    testing can function.

--- lib/init.bash
+++ lib/init.bash
@@ -1,5 +1,5 @@
-source ./lib/action.bash
-source ./lib/config.bash
-source ./lib/event.bash
-source ./lib/snapshot.bash
-source ./lib/timestamp.bash
+source "$LIB_DIR"/action.bash
+source "$LIB_DIR"/config.bash
+source "$LIB_DIR"/event.bash
+source "$LIB_DIR"/snapshot.bash
+source "$LIB_DIR"/timestamp.bash
--- tests/lib/init.bash
+++ tests/lib/init.bash
@@ -1,11 +1,16 @@
+export SELF_NAME=test
+
+export LIB_DIR=$PWD/lib
 export CONFIG_DIR=$TEMP_DIR/etc
 export PROFILES_DIR=$CONFIG_DIR/profile.d
 export DEFAULTS_FILE=$CONFIG_DIR/defaults.conf
 
+export BTRFS_SNAPSHOTS_LIB_DIR=$LIB_DIR
 export BTRFS_SNAPSHOTS_CONFIG_DIR=$CONFIG_DIR
 
-export SELF_NAME=test
 export TIMESTAMP=2001-01-01T00:00:00Z
 
 source ./tests/lib/assert.bash
 source ./tests/lib/timestamp.bash
+
+source ./lib/init.bash
